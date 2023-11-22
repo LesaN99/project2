079e6ce96a63119e17ad4bd002875c89f8e438e3
LesaN99 <149886844+LesaN99@users.noreply.github.com>
2023-11-22 14:42:43 +0200

    updating the js content to something more functional

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 function showTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = response.data.temperature.current;
-  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = response.data.main.temp;
+  let temperature = Math.round(response.data.main.temp);
   let cityElement=document.querySelector("#current-city");
   let descriptionElement=document.querySelector("#description");
   let humidityElement=document.querySelector("#humidity");
   let windSpeedElement=document.querySelector("#wind-speed");
   let iconElement=document.querySelector("#icon");
   
-  windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
-  humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
-  descriptionElement.innerHTML=response.data.condition.description;
+  windSpeedElement.innerHTML=`${response.data.main.pressure}km/h`;
+  humidityElement.innerHTML=`${response.data.main.humidity}%`;
+  descriptionElement.innerHTML=response.data.weather.description;
   cityElement.innerHTML=response.data.city;
   temperatureElement.innerHTML = temperature;
-  iconElement.innerHTML = ` <img src="${response.data.condition.icon_url}" class="current-temperature-icon`;
+  iconElement.innerHTML = ` <img src="${response.data.weather.icon_url}" class="current-temperature-icon`;
 }
 
 function search(event) {
@@ -27,7 +27,7 @@ function search(event) {
 
 function searchCity(city) {
   let apiKey = `2bd326a60dc89a53287e446e819664df`;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(showTemperature);
 }

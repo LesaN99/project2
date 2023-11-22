e501854da4eabba80f150798572f365c380f93e0
LesaN99 <149886844+LesaN99@users.noreply.github.com>
2023-11-22 17:03:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ function search(event) {
   let cityElement = document.querySelector("#current-city");
   let city = searchInputElement.value;
   cityElement.innerHTML = city;
+  searchCity = (searchInputElement.value);
 }
 
 function searchCity(city) {

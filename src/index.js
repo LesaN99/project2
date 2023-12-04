21be32a16753a107e5cdb6bf18ef009063d8da8c
LesaN99 <149886844+LesaN99@users.noreply.github.com>
2023-12-04 18:53:49 +0200

    Added my fontawesome kit's unique embed code into Html,added an icon and then styled icon from syles.css file.

--- src/index.js
+++ src/index.js
@@ -92,7 +92,7 @@ function displayForecast(response) {
         `
         <div class="weather-forecast-day">
         <div class="weather-forecast-date">${formatDay(day.time)}</div>
-        <img src ="${day.condition.icon_url}" class="weather-forecast-icon"/>
+        <img src ="${day.condition.icon_url}" class="weather-forecast-icon" alt="icon"/>
         <div class ="weather-forecast-temperatures">
         <div class ="weather-forecast-temperature-max">
         <strong>${Math.round(day.temperature.maximum)}°</strong>
